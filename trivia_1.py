e683b0ab99e83b83056082535cc4ad0f87a9bcdb
JedFelker <101207499+JedFelker@users.noreply.github.com>
2023-04-06 11:45:47 -0600

    windows

--- trivia_1.py
+++ trivia_1.py
@@ -14,10 +14,19 @@ class Trivia:
     def __init__(self):
         # Create the root window
         self.root = Tk()
+        self.supWindow = Toplevel(self.root)
+        self.movWindow = Toplevel(self.root)
+        self.gamWindow = Toplevel(self.root)
+        self.carWindow = Toplevel(self.root)
         self.root.title("Jed's Trivia Game")
 
         # Prevent window from resizing
         self.root.resizable(False, False)
+        self.root.geometry("275x150")
+        self.supWindow.withdraw()
+        self.gamWindow.withdraw()
+        self.movWindow.withdraw()
+        self.carWindow.withdraw()
 
         # Create the GUI widgets in a seperate method
         self.create_widgets()
@@ -35,33 +44,33 @@ class Trivia:
         self.btn_movies = Button(
             self.main_frame,     # Assign to parent frame
             text="MOVIES",           # Text shown on button
-            width=26
+            width=37,
             # Connect convert method to button click
-            # command=self.
+            command=lambda: [self.movies(), self.root.withdraw()]
         )
 
         self.btn_games = Button(
             self.main_frame,     # Assign to parent frame
             text="GAMES",           # Text shown on button
-            width=26
+            width=37,
             # Connect convert method to button click
-            # command=self.
+            command=lambda: [self.games(), self.root.withdraw()]
         )
 
         self.btn_superheroes = Button(
             self.main_frame,     # Assign to parent frame
             text="SUPERHEROES",           # Text shown on button
-            width=26,
+            width=37,
             # Connect convert method to button click
-            # command=self.
+            command=lambda: [self.superheroes(), self.root.withdraw()]
         )
 
         self.btn_cartoons = Button(
             self.main_frame,     # Assign to parent frame
             text="CARTOONS",           # Text shown on button
-            width=26
+            width=37,
             # Connect convert method to button click
-            # command=self.
+            command=lambda: [self.cartoons(), self.root.withdraw()]
         )
 
         # Set padding between frame and window
@@ -71,6 +80,55 @@ class Trivia:
         self.btn_games.grid(row=1, column=0)
         self.btn_superheroes.grid(row=2, column=0)
         self.btn_cartoons.grid(row=3, column=0)
+
+    def superheroes(self):
+        self.supWindow.deiconify()
+        self.supWindow.title("Superheroes")
+        self.supWindow.geometry("275x150")
+
+        self.newLabel = Label(
+            self.supWindow,
+            
+            text="new window"
+        )
+
+        self.newLabel.grid(row=0, column=0)
+
+    def movies(self):
+        self.movWindow.deiconify()
+        self.movWindow.title("Movies")
+        self.movWindow.geometry("275x150")
+
+        self.newLabel = Label(
+            self.movWindow,
+            text="new window"
+        )
+
+        self.newLabel.grid(row=0, column=0)
+
+    def games(self):
+        self.gamWindow.deiconify()
+        self.gamWindow.title("Movies")
+        self.gamWindow.geometry("275x150")
+
+        self.newLabel = Label(
+            self.gamWindow,
+            text="new window"
+        )
+
+        self.newLabel.grid(row=0, column=0)
+
+    def cartoons(self): 
+        self.carWindow.deiconify()
+        self.carWindow.title("Movies")
+        self.carWindow.geometry("275x150")
+
+        self.newLabel = Label(
+            self.carWindow,
+            text="new window"
+        )
+
+        self.newLabel.grid(row=0, column=0)
 
 
 """Create program objet from the program class to run the program."""
